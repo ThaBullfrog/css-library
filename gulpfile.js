3dcd69d8510ffd06a1a4318b010f1eb1a8614554
ThaBullfrog <th3bullfrog@gmail.com>
2018-01-14 13:26:15 -0500

    add autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
+var files_to_watch = './scss/**/*.scss';
 var input = './scss/main.scss';
 var output = './css';
 var sourcemaps_output = './css/maps';
@@ -15,6 +17,15 @@ gulp.task('sass', function () {
     .src(input)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write(sourcemaps_output))
     .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    .watch(files_to_watch, ['sass'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });

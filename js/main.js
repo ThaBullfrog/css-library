b0215176c4c77d7ac45fe258884b5ac6a179b90a
ThaBullfrog <th3bullfrog@gmail.com>
2018-03-13 18:33:00 -0400

    add on-scroll animations

--- js/main.js
+++ js/main.js
@@ -26,3 +26,55 @@ $(document).ready(function() {
     });
   }
 });
+
+// // Rails only. Keeps animations from firing in turbolinks preview.
+// $( document ).on('turbolinks:render', function() {
+//   if (document.documentElement.hasAttribute("data-turbolinks-preview")) {
+//     var $animated = $(".animated");
+//     $animated.addClass('dont-animate-yet');
+//   }
+// });
+
+var animation_elements = $('.animated');
+var animation_elements_positions = [];
+var win = $(window);
+
+function update_animation_elements_positions() {
+  animation_elements_positions = [];
+  $.each(animation_elements, function() {
+    var element = $(this);
+    animation_elements_positions.push({
+      elem: element, 
+      position: element.offset().top + element.outerHeight() * 0.3
+    });
+  });
+}
+
+$(document).ready(function() {
+  animation_elements = $(".animated");
+  animation_elements.addClass('dont-animate-yet');
+  $(".animated-child").addClass('dont-animate-yet');
+  update_animation_elements_positions();
+  check_if_in_view();
+});
+
+function check_if_in_view() {
+  var window_height = win.height();
+  var window_top_position = win.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+  var array_length = animation_elements_positions.length;
+  for (var i = 0; i < array_length; i++) {
+    var info = animation_elements_positions[i];
+
+    //check to see if this current container is within viewport
+    if ((info.position <= window_bottom_position)) {
+      info.elem.removeClass('dont-animate-yet');
+    }
+  }
+}
+
+win.on('scroll', check_if_in_view);
+win.on('resize', function() {
+  update_animation_elements_positions();
+  check_if_in_view();
+});
